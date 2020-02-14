6ecb8e71e9e69fbee85804b9e6c771f823e4b149
biancapower <bianca@biancapower.com>
2020-02-14 18:37:19 +1100

    update webhook to new stripe api

--- app/controllers/payments_controller.rb
+++ app/controllers/payments_controller.rb
@@ -1,18 +1,33 @@
 class PaymentsController < ApplicationController
     skip_before_action :verify_authenticity_token, only: [:webhook]
     def success
-
+        raise 'e'
     end
 
     def webhook
-        payment_id= params[:data][:object][:payment_intent]
-        payment = Stripe::PaymentIntent.retrieve(payment_id)
-        listing_id = payment.metadata.listing_id
-        user_id = payment.metadata.user_id
 
-        p "listing id " + listing_id
-        p "user id " + user_id
+        event = Stripe::Event.construct_from(
+            params.to_unsafe_h
+        )
 
-        status 200
+        # Handle the event
+        case event.type
+        when 'payment_intent.succeeded'
+            payment_intent = event.data.object # contains a Stripe::PaymentIntent
+            # Then define and call a method to handle the successful payment intent.
+            # handle_payment_intent_succeeded(payment_intent)
+        when 'payment_method.attached'
+            payment_method = event.data.object # contains a Stripe::PaymentMethod
+            # Then define and call a method to handle the successful attachment of a PaymentMethod.
+            # handle_payment_method_attached(payment_method)
+        # ... handle other event types
+        else
+            # Unexpected event type
+            render :nothing => true, :status => :bad_request
+            return
+        end
+
+        # success, but don't need to send anything back to Stripe
+        head :no_content
     end
 end
