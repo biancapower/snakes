2fdaea5dca96773ae8bf4d4c19a691f01001cd96
biancapower <bianca@biancapower.com>
2020-02-14 19:00:08 +1100

    rails g migration AddBoughtToListings bought:boolean

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_02_14_075300) do
+ActiveRecord::Schema.define(version: 2020_02_14_075912) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -55,6 +55,7 @@ ActiveRecord::Schema.define(version: 2020_02_14_075300) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.bigint "user_id"
+    t.boolean "bought"
     t.index ["breed_id"], name: "index_listings_on_breed_id"
     t.index ["user_id"], name: "index_listings_on_user_id"
   end
